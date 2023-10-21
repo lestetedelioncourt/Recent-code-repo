f148d9b8bd1d219325c2ba1c8f22ce84c5ccaee4
lestetedelioncourt <37556899+lestetedelioncourt@users.noreply.github.com>
2023-10-21 13:49:16 +0100

    Update Reference.py

--- Reference.py
+++ Reference.py
@@ -8967,7 +8967,7 @@ test_data = datasets.FashionMNIST(
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
-        super().__init__()=
+        super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(28*28, 512),
