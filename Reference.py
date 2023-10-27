661508765b30f85b6698c3c92d43a4d137cdd015
lestetedelioncourt <37556899+lestetedelioncourt@users.noreply.github.com>
2023-10-27 20:40:40 +0100

    Update Reference.py

--- Reference.py
+++ Reference.py
@@ -14172,3 +14172,24 @@ torchmetric_accuracy = Accuracy(task="multiclass", num_classes=4)
 
 #Calculate accuracy
 print(torchmetric_accuracy(y_preds, y_blob_test).item())
+
+==>EXERCISES<=
+
+# Code for creating a spiral dataset from CS231n
+import numpy as np
+N = 100 # number of points per class
+D = 2 # dimensionality
+K = 3 # number of classes
+X = np.zeros((N*K,D)) # data matrix (each row = single example)
+y = np.zeros(N*K, dtype='uint8') # class labels
+for j in range(K):
+  ix = range(N*j,N*(j+1))
+  #r,t == polar co-ordinates
+  r = np.linspace(0.0,1,N) # radius
+  t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
+  X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
+  #formula for converting polar co-ordinates to cartesian co-ordinates (above)
+  y[ix] = j
+  # lets visualize the data
+  plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+  plt.show()
